76f690e9ef74aa06142f2ba127dc101eb884e7d0
pjh5365 <pjh5365@naver.com>
2023-11-09 17:21:13 +0900

    Init: 프로젝트 생성 및 의존성 추가
    개발을 위해 인텔리제이로 스프링부트 프로젝트를 생성하였다.
    생성과 함께 추가한 의존성은 아래와 같다.
    - Spring Web
    - Lombok
    - Thymeleaf

---
+++ src/main/java/pjh5365/linuxserviceweb/LinuxServiceWebApplication.java
@@ -0,0 +1,13 @@
+package pjh5365.linuxserviceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxServiceWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxServiceWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pjh5365/linuxserviceweb/LinuxServiceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pjh5365.linuxserviceweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxServiceWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
