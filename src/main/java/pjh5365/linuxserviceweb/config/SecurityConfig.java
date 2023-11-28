597376937140222fadaf57e6199466a97c998404
pjh5365 <pjh5365@naver.com>
2023-11-28 15:13:07 +0900

    Fix: 로그인 성공에 대한 처리 버그수정
    로그인에 성공하면 자꾸 다른 url 로 연결되는 경우가 생겨 로그인 성공핸들러를 따로 만들어 무조건 / 로 이동하도록 수정하였다.
    
    Related to: #10

--- src/main/java/pjh5365/linuxserviceweb/config/SecurityConfig.java
+++ src/main/java/pjh5365/linuxserviceweb/config/SecurityConfig.java
@@ -7,17 +7,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import pjh5365.linuxserviceweb.handler.CustomAuthenticationFailure;
+import pjh5365.linuxserviceweb.handler.CustomAuthenticationFailureHandler;
+import pjh5365.linuxserviceweb.handler.CustomAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final CustomAuthenticationFailure customAuthenticationFailure;
+    private final CustomAuthenticationFailureHandler failureHandler;
+    private final CustomAuthenticationSuccessHandler successHandler;
 
     @Autowired
-    public SecurityConfig(CustomAuthenticationFailure customAuthenticationFailure) {
-        this.customAuthenticationFailure = customAuthenticationFailure;
+    public SecurityConfig(CustomAuthenticationFailureHandler customAuthenticationFailureHandler, CustomAuthenticationSuccessHandler successHandler) {
+        this.failureHandler = customAuthenticationFailureHandler;
+        this.successHandler = successHandler;
     }
 
     @Bean
@@ -38,12 +41,15 @@ public class SecurityConfig {
         http.csrf((csrf) -> csrf.disable());    // csrf 는 우선 비활성화
 
         http.formLogin((auth) -> auth
-                        .loginPage("/login")
-                        .loginProcessingUrl("/loginProc")
-                        .failureHandler(customAuthenticationFailure)
-                        .defaultSuccessUrl("/")
+                .loginPage("/login")
+                .loginProcessingUrl("/loginProc")
+                .successHandler(successHandler)
+                .failureHandler(failureHandler)
         );
 
+        http.logout((auth) -> auth
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/"));
         return http.build();
     }
 }
---
+++ src/main/java/pjh5365/linuxserviceweb/handler/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,18 @@
+package pjh5365.linuxserviceweb.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        getRedirectStrategy().sendRedirect(request, response, "/"); // 다른 페이지로 이동하지 않고 무조건 / 페이지로만 이동하도록 설정
+    }
+}
