3b6546a835eee8b7802df7313560c6ecc8a49272
pjh5365 <pjh5365@naver.com>
2023-11-30 19:32:47 +0900

    Refactor: 로그를 불러와 파싱하는 부분 수정
    파싱이 정확하게 되지않아 수정함
    
    Related to: #13

--- src/main/java/pjh5365/linuxserviceweb/domain/log/NginxAccessLog.java
+++ src/main/java/pjh5365/linuxserviceweb/domain/log/NginxAccessLog.java
@@ -16,16 +16,18 @@ public class NginxAccessLog extends Log {
     private String time = "";
     private String http = "";
     private String status = "";
+    private String request = "";
     private String client = "";
 
     public NginxAccessLog() {
     }
 
-    public NginxAccessLog(String ip, String time, String http, String status, String client) {
+    public NginxAccessLog(String ip, String time, String http, String status, String request, String client) {
         this.ip = ip;
         this.time = time;
         this.http = http;
         this.status = status;
+        this.request = request;
         this.client = client;
     }
 
@@ -57,6 +59,7 @@ public class NginxAccessLog extends Log {
         StringBuilder timeSb = new StringBuilder(); // 접속시각 정보를 담을 빌더
         StringBuilder httpSb = new StringBuilder(); // http 정보를 담을 빌더
         StringBuilder statusSb = new StringBuilder();   // 응답상태를 담을 빌더
+        StringBuilder requestSb = new StringBuilder();   // 응답상태를 담을 빌더
         StringBuilder clientSb = new StringBuilder();   // 클라이언트의 정보를 담을 빌더
 
         // 파일에 내용이 없는 경우를 대비해 빈 배열을 생성
@@ -81,23 +84,28 @@ public class NginxAccessLog extends Log {
                 // 5,6,7 : http 방법
                 // 8 : 상태
                 // 9 : 응답크기
-                // 11~17 : 클라이언트 정보
+                // 10 : 요청주소
+                // 나머지 클라이언트 정보
 
                 ipSb.append(tokens[0]).append("\n");
                 timeSb.append(tokens[3]).append(tokens[4]).append("\n");
                 httpSb.append(tokens[5]).append(tokens[6]).append(tokens[7]).append("\n");
                 statusSb.append(tokens[8]).append("\n");
-                clientSb.append(tokens[11]).append(tokens[12]).append(tokens[13]).append(tokens[14]).append(tokens[15]).append(tokens[16]).append(tokens[17]).append("\n");
+                requestSb.append(tokens[10]).append("\n");
+                for(int j = 11; j < tokens.length; j++)
+                    clientSb.append(tokens[j]).append(" ");
+                clientSb.append("\n");
             }
             String[] ip = ipSb.toString().split("\n"); // ip 정보를 읽은 내용을 배열로 한줄씩 저장
             String[] time = timeSb.toString().split("\n"); // 시각 정보를 읽은 내용을 배열로 한줄씩 저장
             String[] http = httpSb.toString().split("\n"); // http 정보를 읽은 내용을 배열로 한줄씩 저장
             String[] status = statusSb.toString().split("\n"); // 상태 정보를 읽은 내용을 배열로 한줄씩 저장
+            String[] request = requestSb.toString().split("\n"); // 상태 정보를 읽은 내용을 배열로 한줄씩 저장
             String[] client = clientSb.toString().split("\n"); // 클라이언트 정보를 읽은 내용을 배열로 한줄씩 저장
 
             logs = new NginxAccessLog[ip.length];    // 로그의 길이만큼 배열 생성
             for(int i = 0; i < ip.length; i++) {
-                logs[i] = new NginxAccessLog(ip[i], time[i], http[i], status[i], client[i]);
+                logs[i] = new NginxAccessLog(ip[i], time[i], http[i], status[i], request[i], client[i]);
             }
         }
         return logs;   // 읽어온 내용을 리턴, 없다면 빈칸을 리턴
