5b6c225721f4a28ee61efca8dca9d274bb82edb2
pjh5365 <pjh5365@naver.com>
2023-11-30 19:16:23 +0900

    Feat: 방생성 버튼 추가
    채팅방을 생성할 수 있는 버튼을 추가하고 이에 맞게 컨트롤러를 수정함
    
    Related to: #12

--- src/main/java/pjh5365/linuxserviceweb/controller/ChatController.java
+++ src/main/java/pjh5365/linuxserviceweb/controller/ChatController.java
@@ -31,14 +31,13 @@ public class ChatController {
 
     @GetMapping("/room/{roomId}")
     public String getRoom(@PathVariable String roomId, Model model, @AuthenticationPrincipal UserDetails userDetails) {
-
         model.addAttribute("room", chatRoomRepository.findRoomById(roomId));
         model.addAttribute("userDetails", userDetails);  // 시큐리티에서 사용자 정보받아 타임리프로 넘기기
         return "room";  // 개인 채팅방 리턴하기
     }
 
-    @PostMapping("/room/{roomName}")   // 채팅방 생성
-    public String createRoom(@PathVariable String roomName) {
+    @PostMapping("/room")   // 채팅방 생성
+    public String createRoom(String roomName) {
         chatRoomRepository.createChatRoomDto(roomName);
 
         return "redirect:/chat/rooms";  // 채팅방 생성 후 채팅방 리스트로 이동
