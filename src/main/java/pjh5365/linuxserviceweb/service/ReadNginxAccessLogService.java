00334b32c9c29d6636b1005818001752e853f152
pjh5365 <pjh5365@naver.com>
2023-12-01 10:48:53 +0900

    Chore: 로그 메일 전송기능 수정
    원래 정해진 주소로만 로그를 전송하였는데 이제 관리자 권한을 가진 사용자 정보를 읽어 사용자별로 메일을 전송하도록 수정함
    
    Related to: #16

--- src/main/java/pjh5365/linuxserviceweb/service/ReadNginxAccessLogService.java
+++ src/main/java/pjh5365/linuxserviceweb/service/ReadNginxAccessLogService.java
@@ -1,11 +1,25 @@
 package pjh5365.linuxserviceweb.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import pjh5365.linuxserviceweb.domain.log.NginxAccessLog;
 import pjh5365.linuxserviceweb.domain.mail.Mail;
+import pjh5365.linuxserviceweb.domain.user.UserEntity;
+import pjh5365.linuxserviceweb.repository.UserRepository;
+
+import java.util.Optional;
 
 @Service
 public class ReadNginxAccessLogService implements ReadLogService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public ReadNginxAccessLogService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public NginxAccessLog[] loadLog() {
@@ -25,7 +39,13 @@ public class ReadNginxAccessLogService implements ReadLogService {
         String title = "nginx access.log 파일 백업";
         String logFilePath = "/home/pibber/log/nginx/access/";
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Optional<UserEntity> optionalUser = userRepository.findByUsername(authentication.getName());
+        UserEntity user = optionalUser.get();
+
+        this.copyLog(); // 파일 전송 전 백업부터
+
         Mail mail = new Mail();
-        mail.sendLogMail(title, logFilePath);
+        mail.sendLogMail(user.getEmail(), title, logFilePath);
     }
 }
